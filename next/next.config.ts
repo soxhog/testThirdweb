87d875a361438403f7c132b91231feb968c33e73
soxhog <soranatsu.12250511@gmail.com>
2024-11-13 19:49:39 +0900

    add cors

--- next/next.config.ts
+++ next/next.config.ts
@@ -3,6 +3,27 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://jolly-zabaione-4ab98e.netlify.app", // 特定のオリジンを許可
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-Requested-With, Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
