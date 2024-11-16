903a8bcd53c422b4127f7889fb60e56b4118ea16
soxhog <soranatsu.12250511@gmail.com>
2024-11-16 18:47:45 +0900

    add metamask connect button

--- next/src/pages/index.tsx
+++ next/src/pages/index.tsx
@@ -2,6 +2,13 @@ import { ConnectButton } from "thirdweb/react";
 import { createWallet } from "thirdweb/wallets";
 import { ethereum } from "thirdweb/chains";
 import { client } from "@/utils/thirdweb.helper";
+import { useState } from "react";
+
+declare global {
+  interface Window {
+    ethereum?: any;
+  }
+}
 
 export default function Home() {
   const wallets = [
@@ -12,6 +19,45 @@ export default function Home() {
     createWallet("io.zerion.wallet"),
     createWallet("com.trustwallet.app"),
   ];
+  const [isConnected, setIsConnected] = useState(false);
+  const [walletAddress, setWalletAddress] = useState("");
+
+  const getEthereum = () => {
+    if (typeof window === "undefined") {
+      console.log("Oops, `window` is not defined");
+      throw new Error("Error: no window");
+    }
+    const { ethereum } = window;
+    return ethereum;
+  };
+  const walletConnect = async () => {
+    const ethereum = getEthereum();
+    if (!ethereum) {
+      window.open(`https://metamask.app.link/dapp/${window.location.host}/#/`);
+      return;
+    }
+
+    if (isConnected) {
+      alert("Wallet is already connected!");
+      return;
+    }
+    await ethereum
+      .request({ method: "eth_requestAccounts" })
+      .then(async () => {
+        const accounts = await ethereum.request({ method: "eth_accounts" });
+        setWalletAddress(accounts[0]);
+        setIsConnected(true);
+      })
+      .catch((err: any) => {
+        if (err.code === 4001) {
+          alert("User rejected the connection!");
+          return;
+        } else {
+          alert("An error occured while connecting!");
+          return;
+        }
+      });
+  };
   return (
     <div className="flex flex-col items-center justify-center h-screen space-y-4">
       <h1 className="text-4xl font-bold">Child Page</h1>
@@ -36,6 +82,14 @@ export default function Home() {
         }}
         wallets={wallets}
       />
+      <button
+        className="bg-blue-500 text-white p-2 rounded-md"
+        disabled={isConnected}
+        onClick={walletConnect}
+      >
+        MetaMask Connect Wallet
+      </button>
+      <p>Wallet Address: {walletAddress}</p>
     </div>
   );
 }
