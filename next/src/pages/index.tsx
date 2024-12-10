1d80a02d3419f5c43f6ce73d086688e321e40460
soxhog <soranatsu.12250511@gmail.com>
2024-12-10 21:17:46 +0900

    Refactor wallet connection to use SDK

--- next/src/pages/index.tsx
+++ next/src/pages/index.tsx
@@ -1,41 +1,35 @@
 import { useState } from "react";
-import { ethers } from "ethers";
+import { useSDK } from "@metamask/sdk-react";
 
 const Home = () => {
   const [walletAddress, setWalletAddress] = useState<string | null>(null);
   const [error, setError] = useState<string | null>(null);
+  const { sdk, connected, connecting, provider, chainId } = useSDK();
 
-  const connectWallet = async () => {
+  const connect = async () => {
     try {
-      // MetaMask がブラウザに存在するか確認
-      if (!window.ethereum) {
-        setError("MetaMask がインストールされていません。");
-        return;
-      }
-
-      // MetaMask に接続
-      const provider = new ethers.BrowserProvider(window.ethereum);
-      await provider.send("eth_requestAccounts", []);
-      const signer = await provider.getSigner();
-      const address = await signer.getAddress();
-
-      // ウォレットアドレスを状態に保存
-      setWalletAddress(address);
-      setError(null); // エラーをクリア
-    } catch (err: any) {
-      setError(err.message || "ウォレット接続に失敗しました。");
+      const accounts = await sdk?.connect();
+      setWalletAddress(accounts?.[0]);
+    } catch (err) {
+      console.warn("failed to connect..", err);
+      setError("failed to connect..");
     }
   };
 
   return (
     <div style={{ textAlign: "center", padding: "20px" }}>
       <h1>Connect to MetaMask</h1>
-      {walletAddress ? (
+      <button style={{ padding: 10, margin: 10 }} onClick={connect}>
+        Connect
+      </button>
+      {connected && (
         <div>
-          <p>ウォレットアドレス: {walletAddress}</p>
+          <>
+            {chainId && `Connected chain: ${chainId}`}
+            <p></p>
+            {walletAddress && `Connected account: ${walletAddress}`}
+          </>
         </div>
-      ) : (
-        <button onClick={connectWallet}>Connect Wallet</button>
       )}
       {error && <p style={{ color: "red" }}>{error}</p>}
     </div>
